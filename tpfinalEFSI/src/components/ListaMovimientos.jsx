e01fd310542ffcaa494b9f03e4eb26b61082fa5f
LIAM COHEN <48796139@ort>
2025-10-22 10:39:54 -0300

    agregar filtros de búsqueda y eliminación de movimientos

--- tpfinalEFSI/src/components/ListaMovimientos.jsx
+++ tpfinalEFSI/src/components/ListaMovimientos.jsx
@@ -1,12 +1,14 @@
 import MovimientoItem from './MovimientoItem'
 
-export default function ListaMovimientos({ movimientos }){
+export default function ListaMovimientos({ movimientos, onDelete }){
   if (!movimientos || movimientos.length === 0){
-    return <div className="card helper">No hay movimientos aún.</div>
+    return <div className="card helper">No hay movimientos para mostrar.</div>
   }
   return (
     <div className="list">
-      {movimientos.map(m => <MovimientoItem key={m.id} m={m} />)}
+      {movimientos.map(m => (
+        <MovimientoItem key={m.id} m={m} onDelete={onDelete} />
+      ))}
     </div>
   )
 }
